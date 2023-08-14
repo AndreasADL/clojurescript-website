72c1c448d5b913470cde5284e6561153845e31bb
Andreas <ahrghhh@t-online.de>
2023-08-14 22:23:13 +0200

    fixed and used deploy.ps1 script

--- deploy.ps1
+++ deploy.ps1
@@ -3,8 +3,10 @@ lein clean
 lein fig:deploy
 
 # Copy changes in css, images and index.html to docs
-Copy-Item .\resources\public\css -r .\docs
-Copy-Item .\resources\public\images -r images
+Remove-Item -r .\docs\css
+Copy-Item -r .\resources\public\css .\docs
+Remove-Item -r .\docs\images
+Copy-Item -r .\resources\public\images .\docs
 Copy-Item .\resources\public\index.html .\docs
 
 # Finish message
