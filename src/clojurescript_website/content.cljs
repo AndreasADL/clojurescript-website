9471ce63bcd05636e1cd5bfc7eaafb905dc54ea5
Andreas <ahrghhh@t-online.de>
2023-08-14 22:02:48 +0200

    added content.cljs

--- src/clojurescript_website/content.cljs
+++ src/clojurescript_website/content.cljs
@@ -1,1 +1,28 @@
-(ns clojurescript-website.content)
+(ns clojurescript-website.content
+  (:require
+   [reagent.core :as reagent :refer [atom]]))
+
+;; Application state
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defonce app-state (atom {:text "How does app-state work?!"}))
+
+;; Content components
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn main-page []
+ [:section {:class "section"}
+ 
+   [:h1
+    {:class "title"}
+    "Welcome to ClojureBridge"]
+ 
+    ;; Content will just use HTML tags directly, useful when you have no specific styles
+    ;; https://bulma.io/documentation/elements/content/
+ 
+   [:div {:class "content"}
+    [:p
+     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed enim ante. Nullam consectetur, sapien in rutrum facilisis, augue velit finibus est, at lobortis odio eros sollicitudin risus. Nullam mollis, metus a varius volutpat, metus elit mollis est, finibus pretium dui enim non velit. Praesent sit amet volutpat nulla. Sed volutpat venenatis nisi id sagittis. Nunc nec efficitur mi. Duis consequat sapien ultricies quam bibendum, elementum faucibus sapien bibendum. Morbi diam elit, gravida iaculis metus vitae, ullamcorper mattis mi. Maecenas luctus lorem metus. Maecenas eleifend nisl sit amet felis accumsan, sit amet tincidunt turpis consequat. Cras non molestie ante, a pellentesque dui."]
+    [:p
+     "Vivamus ullamcorper at orci ac tincidunt. Vivamus tincidunt sed erat nec consequat. Donec venenatis lorem justo, eget imperdiet arcu ultrices vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec congue tempor posuere. Sed nec nisl mauris. Maecenas elementum quam justo, vitae auctor felis dapibus a. Phasellus leo justo, mattis a auctor tempus, facilisis vel tellus. Etiam at scelerisque justo, ac facilisis purus. Duis in leo pretium purus bibendum ultricies ac vitae lectus. Proin nec mi nec urna sollicitudin iaculis. In a orci felis. Sed luctus posuere luctus. Cras id euismod orci, id mollis nibh. Vestibulum et tellus quis lorem placerat scelerisque non et nisl. Ut dictum lacus nulla, sit amet ultricies eros pharetra vitae. "]
+    ]])
--- src/clojurescript_website/core.cljs
+++ src/clojurescript_website/core.cljs
@@ -2,25 +2,40 @@
   (:require
    [goog.dom :as gdom]
    [reagent.core :as reagent :refer [atom]]
-   [reagent.dom :as rdom]))
+   [reagent.dom :as rdom]
+   [clojurescript-website.content :as content]
+   ))
 
-(println "This text is printed from src/clojurescript_website/core.cljs. Go ahead and edit it and see reloading in action.")
+;; simple debug statement for each build
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn multiply [a b] (* a b))
+(println (js/Date.) "Reloading: core.cljs")
+
+;; Application state
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; define your app data so that it doesn't get over-written on reload
 (defonce app-state (atom {:text "Hello world!"}))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+;; Helper functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/clojurescript_website/core.cljs and watch it change!"]])
+(defn multiply [a b] (* a b))
+
+;; Content components
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn landing-page []
+  [content/main-page])
+
+;; System
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn mount [el]
-  (rdom/render [hello-world] el))
+  (rdom/render [landing-page] el))
+
+(defn get-app-element []
+  (gdom/getElement "app"))
 
 (defn mount-app-element []
   (when-let [el (get-app-element)]
@@ -36,4 +51,4 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
